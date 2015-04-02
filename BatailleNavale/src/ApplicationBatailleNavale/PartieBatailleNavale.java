3d395a76a142be8f02146251535375e75f07eb17
Laure Cayre <laure.cayre@iut-valence.fr>
2015-04-02 11:31:38 +0200

    Modification de la classe BatailleNavale
    - ajout de documentation

--- BatailleNavale/src/ApplicationBatailleNavale/PartieBatailleNavale.java
+++ BatailleNavale/src/ApplicationBatailleNavale/PartieBatailleNavale.java
@@ -1,5 +1,35 @@
 package ApplicationBatailleNavale;
 
-public class PartieBatailleNavale {
+/**
+ * Classe "PartieBatailleNavale" principale de l'application
+ * classe définissant les caractéristiques principales de la partie. 
+ */
 
+public class PartieBatailleNavale
+{
+/* faire le constructeur
+ *
+ * 
+ */
+	
+/* faire la méthode jouer -->*/
+	public void JouerUnePartie()
+	{
+/**
+ * déroulement de la partie*
+ * 
+ * initialiser les tableaux (x4)
+ * 			2 par joueurs:
+ * 				 un pour les bateau placés,
+ * 				 un pour les bateaux shuntés*
+ *  Colonne en lettres de A à J --> Nb_Colonne=10;
+ *  Ligne en chiffres de 1 à 10 --> Nb_Ligne=10;
+ *  
+ * placer les bateaux*
+ * Définir les types de bateau
+ * 		bateau 1 case(x1); 2 cases(x2); 3 cases(x2); 4 cases (x1)
+ * 
+ **/
+	
+	}
 }
