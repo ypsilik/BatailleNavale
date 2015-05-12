9faaf274e79486d3269b6de854a9264476c261a9
Laure Cayre <laure.cayre@iut-valence.fr>
2015-05-12 09:45:20 +0200

    modif grid

--- BatailleNavale/src/ApplicationBattleship/Grid.java
+++ BatailleNavale/src/ApplicationBattleship/Grid.java
@@ -1,39 +1,70 @@
 package ApplicationBattleship;
+
+import BatailleNavaleVersionCours.Ship;
+
 /**
  * Class to create grid for battleship
  * @author CAYRE Laure and LAURENT Maud
  *
  */
 public class Grid {
+	
 	/**
-	 * attribute to initialize columns
-	 */
-	private final static int COLUMNS = 11;
-	/**
-	 * private attribute to initialize lines
-	 */
-	private final static int LINES = 11;
-	/**
-	 * private attribute with ship type
-	 */
+	  private attribute with ship type
+	 **/
 	private Ship ship;
+
 	/**
-	 * private grid 2 dimensions
+	 *  initialisation NB_lines
 	 */
-	private int[][] grid;
+	private final static int NB_Lines=11;
+	
 	/**
-	 * Constructor for grid without parameters
+	 * initialisation NB_column 
 	 */
+	private final static int NB_Column=11;
+	 
+	private char[][] grid;
+	
 	public Grid() {
-		this.grid = new int[LINES][COLUMNS];
-	}
-	/**
-	 * Constructor for grid
-	 * @param Ships position ship in grid
-	 */
-	public Grid(Ship Ships) {
+		this.grid = new char[NB_Lines][NB_Column];
 		
+		for (int i=0; i<NB_Lines ; i++)
+		{
+			for (int j=0; j< NB_Column; j++)
+			{
+				grid[i][j]='~';
+			}
+		}
+			
 	}
 	
-
+	public void DisplayGrid()
+	/*public String toString()*/
+	{
+		
+		{
+			System.out.format("      ");
+			for(int i = 1; i<=NB_Lines;i++ )
+			   {
+			       System.out.format("%4d",i);
+			   }
+			   System.out.println();
+			   System.out.println("--------------------------------------------------");
+			   int n=0;
+			   for(int i = 1 ;i<=NB_Lines;i++) 
+			   {
+				   char code='A';
+				   int increm=code+n;
+				   System.out.format("%4s |",(char)increm);
+				   n++;
+				   for(int j=1;j<=NB_Column;j++) 
+				   {
+					   System.out.format("%4s",(char)126);
+				   }
+				   System.out.println();
+				}
+			}
+	}
 }
+
