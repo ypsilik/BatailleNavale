a1c0310b8941ab3e66c7fcecffbb86942cd3182e
Laurent Maud <maud.laurent@iut-valence.fr>
2015-05-12 16:31:00 +0200

    modification grid et ship

--- BatailleNavale/src/ApplicationBattleship/Ship.java
+++ BatailleNavale/src/ApplicationBattleship/Ship.java
@@ -6,5 +6,22 @@ package ApplicationBattleship;
  *
  */
 public class Ship {
+	private boolean isTouch;
+	private boolean isPlouf;
 	
+	public Ship()
+	{
+		this.isTouch=false;
+		this.isPlouf=false;
+	}
+	
+	public void ShipTouch()
+	{
+		this.isTouch=true;
+	}
+	
+	public void ShipPlouf()
+	{
+		this.isPlouf=true;
+	}
 }
